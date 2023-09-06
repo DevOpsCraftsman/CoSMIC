7bb111d544f7ecc1ccdf51cf0da192169582fa3f
Yahya Abou Imran <alexandre.poitevin.dev@gmail.com>
2023-09-06 07:15:50 +0100

    Simplify layout

--- cosmic/examples/templates/generate.rb
+++ cosmic/examples/templates/generate.rb
@@ -2,7 +2,7 @@ require "erb"
 
 examples_folder = 'cosmic/examples'
 
-erb_file = "#{examples_folder}/templates/abstraction/abstraction.md.erb"
+erb_file = "#{examples_folder}/templates/abstraction/template.md.erb"
 
 erb_str = File.read(erb_file)
 
@@ -19,11 +19,14 @@ concepts = [
   @spec = spec
   @samples = []
   concepts.each do |concept|
+    
+    ext = spec[:extension]
+    lang = spec[:language]
 
-    concept_folder = "#{examples_folder}/templates/#{concept}"
-    sample_files = Dir["#{concept_folder}/#{concept}_sample_*.#{spec[:extension]}"]
+    samples_folder = "#{examples_folder}/templates/#{concept}/#{lang}/*.#{ext}"
+    samples_files = Dir[samples_folder]
 
-    sample_files.each do |sample_file|
+    samples_files.each do |sample_file|
       File.open(sample_file) do |sample|
         @samples.push sample.read
       end
